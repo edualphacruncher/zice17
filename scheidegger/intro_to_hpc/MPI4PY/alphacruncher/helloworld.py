e551a35a38da496d68377a76357055f6261376e6
Simon <simon.scheidegger@gmail.com>
2017-01-27 15:20:45 +0100

    added sample codes

---
+++ scheidegger/intro_to_hpc/MPI4PY/alphacruncher/helloworld.py
@@ -0,0 +1,7 @@
+#!/apps/python/3.5.1/bin/python
+#hello.py
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = MPI.COMM_WORLD.Get_size()
+print "hello world from process ", rank, " from total ", size , "processes"
